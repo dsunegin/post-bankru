a1ded3f289771dd309a8a17c6524c7ec5c37e21e
Denis Sunegin <dsunegin@gmail.com>
2021-03-11 19:30:25 +0000

    Initial commit

--- pm2-post-bankru.sh
+++ pm2-post-bankru.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Start at 8:05 am
+# Start at 06:00 am
 
 pm2 delete post-bankru
-pm2 start post-bankru.js --name post-bankru --cron "5 11 * * *"
+CRON="00 06 * * *" pm2 start post-bankru.js --name post-bankru
 pm2 save
